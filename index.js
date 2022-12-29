c2ddd41376217eb7a7bd6564f595de14e6206b3f
sonimohit481 <sonimohit481@gmail.com>
2022-12-29 17:13:55 +0530

    minor changes

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors({ origin: "*" }));
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// ---------
+app.get("/", (req, res) => {
+    let arr = []
+    arr[0] = {
+        name: "home",
+        lat: Math.random() * (24 - 21) + 21,
+        lng: Math.random() * (84 - 81) + 81,
+    }
+    for (let i = 0; i < 2; i++) {
+        arr[i + 1] = {
+            name: i + 1,
+            lat: Math.random() * (24 - 21) + 21,
+            lng: Math.random() * (84 - 81) + 81,
+        }
+    }
+    arr.push(arr[0])
+    res.send(JSON.stringify(arr))
+})
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log("Listening on post 8080")
+});
